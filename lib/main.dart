8950b07b42dd4eb0ae5fa849437e6958cde593da
Nihad Jusović <76163793+tihynihy@users.noreply.github.com>
2023-04-25 22:35:12 +0200

    Revert "Splash Screen Added | Logo Splash Animated | Added Homepage feature f…"

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
-import 'package:clubspace/splash/splash_design.dart';
 import 'package:flutter/material.dart';
 //Import the dart files
 
 
 
- void main() => runApp(new SplashScreen());
+//void main() => runApp(new MyApp());
 //test them here in the runApp as a parameter!
 
